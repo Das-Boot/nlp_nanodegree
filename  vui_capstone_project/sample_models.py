5b579ddc4e1e726fde443864733e828cdb121cc7
Zhaoning Li <lizhn7@mail2.sysu.edu.cn>
2019-02-23 20:15:35 +0800

    Add Project

---
+++  vui_capstone_project/sample_models.py
@@ -0,0 +1,224 @@
+from keras import backend as K
+from keras.models import Model
+from keras.layers import (BatchNormalization, Conv1D, Dense, Input, 
+    TimeDistributed, Activation, Bidirectional, SimpleRNN, GRU, LSTM, concatenate)
+from keras.engine.topology import Layer
+
+
+class Attention(Layer):
+
+    def __init__(self, nb_head, size_per_head, **kwargs):
+        self.nb_head = nb_head
+        self.size_per_head = size_per_head
+        self.output_dim = nb_head*size_per_head
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.WQ = self.add_weight(name='WQ', 
+                                  shape=(input_shape[0][-1], self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        self.WK = self.add_weight(name='WK', 
+                                  shape=(input_shape[1][-1], self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        self.WV = self.add_weight(name='WV', 
+                                  shape=(input_shape[2][-1], self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        self.WO = self.add_weight(name='WO', 
+                                  shape=(self.output_dim, self.output_dim),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        super(Attention, self).build(input_shape)   
+            
+    def call(self, x):
+        Q_seq,K_seq,V_seq = x
+        Q_seq = K.dot(Q_seq, self.WQ)
+        Q_seq = K.reshape(Q_seq, (-1, K.shape(Q_seq)[1], self.nb_head, self.size_per_head))
+        Q_seq = K.permute_dimensions(Q_seq, (0,2,1,3))
+        K_seq = K.dot(K_seq, self.WK)
+        K_seq = K.reshape(K_seq, (-1, K.shape(K_seq)[1], self.nb_head, self.size_per_head))
+        K_seq = K.permute_dimensions(K_seq, (0,2,1,3))
+        V_seq = K.dot(V_seq, self.WV)
+        V_seq = K.reshape(V_seq, (-1, K.shape(V_seq)[1], self.nb_head, self.size_per_head))
+        V_seq = K.permute_dimensions(V_seq, (0,2,1,3))
+        A = K.batch_dot(Q_seq, K_seq, axes=[3,3]) / self.size_per_head**0.5
+        A = K.permute_dimensions(A, (0,3,2,1))
+        A = K.permute_dimensions(A, (0,3,2,1))    
+        A = K.softmax(A)
+        O_seq = K.batch_dot(A, V_seq, axes=[3,2])
+        O_seq = K.permute_dimensions(O_seq, (0,2,1,3))
+        O_seq = K.reshape(O_seq, (-1, K.shape(O_seq)[1], self.output_dim))
+        O_seq = K.dot(O_seq, self.WO)
+        return O_seq
+        
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0], input_shape[0][1], self.output_dim)
+
+    
+def simple_rnn_model(input_dim, output_dim=29):
+    """ Build a recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add recurrent layer
+    simp_rnn = GRU(output_dim, return_sequences=True, 
+                 implementation=2, name='rnn')(input_data)
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(simp_rnn)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+def rnn_model(input_dim, units, activation, output_dim=29):
+    """ Build a recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add recurrent layer
+    simp_rnn = GRU(units, activation=activation,
+        return_sequences=True, implementation=2, name='rnn')(input_data)
+    # TODO: Add batch normalization 
+    bn_rnn = BatchNormalization(name='bn')(simp_rnn)
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim), name='dense')(bn_rnn) 
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+
+def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
+    conv_border_mode, units, output_dim=29):
+    """ Build a recurrent + convolutional network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # Add convolutional layer
+    conv_1d = Conv1D(filters, kernel_size, 
+                     strides=conv_stride, 
+                     padding=conv_border_mode,
+                     activation='relu',
+                     name='conv1d')(input_data)
+    # Add batch normalization
+    bn_cnn = BatchNormalization(name='bn_conv_1d')(conv_1d)
+    # Add a recurrent layer
+    simp_rnn = GRU(units, activation='relu',
+        return_sequences=True, implementation=2, name='rnn')(bn_cnn)
+    # TODO: Add batch normalization
+    bn_rnn = BatchNormalization(name='bn_rnn')(simp_rnn)
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim), name='dense')(bn_rnn) 
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: cnn_output_length(
+        x, kernel_size, conv_border_mode, conv_stride)
+    print(model.summary())
+    return model
+
+def cnn_output_length(input_length, filter_size, border_mode, stride,
+                       dilation=1):
+    """ Compute the length of the output sequence after 1D convolution along
+        time. Note that this function is in line with the function used in
+        Convolution1D class from Keras.
+    Params:
+        input_length (int): Length of the input sequence.
+        filter_size (int): Width of the convolution kernel.
+        border_mode (str): Only support `same` or `valid`.
+        stride (int): Stride size used in 1D convolution.
+        dilation (int)
+    """
+    if input_length is None:
+        return None
+    assert border_mode in {'same', 'valid'}
+    dilated_filter_size = filter_size + (filter_size - 1) * (dilation - 1)
+    if border_mode == 'same':
+        output_length = input_length
+    elif border_mode == 'valid':
+        output_length = input_length - dilated_filter_size + 1
+    return (output_length + stride - 1) // stride
+
+def deep_rnn_model(input_dim, units, recur_layers, output_dim=29):
+    """ Build a deep recurrent network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Add recurrent layers, each with batch normalization
+    simp_rnn = GRU(units, activation='relu',
+            return_sequences=True, implementation=2, name='rnn_1')(input_data)
+    bn_rnn = BatchNormalization(name='bn_1')(simp_rnn)
+    for rl in range(2, recur_layers+1, 1):
+        simp_rnn = GRU(units, activation='relu',
+            return_sequences=True, implementation=2, name='rnn_%d' % rl)(bn_rnn)
+        bn_rnn = BatchNormalization(name='bn_%d' % rl)(simp_rnn)
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim), name='dense')(bn_rnn) 
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+def bidirectional_rnn_model(input_dim, units, output_dim=29):
+    """ Build a bidirectional recurrent network for speech
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Add bidirectional recurrent layer
+    bidir_rnn = Bidirectional(GRU(units, activation='relu',
+            return_sequences=True, implementation=2), merge_mode='concat', name='bidir_rnn')(input_data)
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim), name='dense')(bidir_rnn) 
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+def final_model(input_dim, filters, kernel_size, conv_stride,
+    conv_border_mode, units, nb_head, size_per_head, output_dim=29):
+    """ Build a deep network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Specify the layers in your network
+    # Add convolutional layer
+    conv_1d = Conv1D(filters, kernel_size, 
+                     strides=conv_stride, 
+                     padding=conv_border_mode,
+                     activation='relu',
+                     name='conv1d')(input_data)
+    # Add batch normalization
+    bn_cnn = BatchNormalization(name='bn_conv_1d')(conv_1d)
+    # Add a multi head self-attention layer
+    mhsa = Attention(nb_head, size_per_head, name='mhsa')([bn_cnn, bn_cnn, bn_cnn])
+    # Add batch normalization
+    bn_mhsa = BatchNormalization(name='bn_mhsa')(mhsa)
+    context = concatenate([bn_cnn, bn_mhsa], axis=-1, name='context')
+    # Add a bidirectional recurrent layer
+    bidir_rnn = Bidirectional(GRU(units, activation='relu',
+            return_sequences=True, implementation=2), merge_mode='concat', name='bidir_rnn')(context)
+    # Add batch normalization
+    bn_bidir_rnn = BatchNormalization(name='bn_bidir_rnn')(bidir_rnn)
+    # Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim), name='dense')(bn_bidir_rnn) 
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: cnn_output_length(
+        x, kernel_size, conv_border_mode, conv_stride)
+    print(model.summary())
+    return model
--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# nlp_nanodegree
+[//]: # (Image References)
+
+[image1]: ./ vui_capstone_project/images/pipeline.png "ASR Pipeline"
+[image2]: ./ vui_capstone_project/images/loss.png "Loss"
+
+# Natural Language Processing Nanodegree Project
+
+## Inroduction
+
+Collection of python NLP projects created for Udacity NLP Nanodegree course. 
+It have practical projects in the field of NLP using modern libraries and frameworks such as Keras and Tensorflow.
+
+## Setup
+
+### Install
+- Pomegranate 
+- Python 3
+- NumPy
+- TensorFlow 1.x
+- Keras 2.x
+
+### Start
+
+All projects are within a [Jupyter Notebook](http://jupyter.org/). To start the notebook, run the command `jupyter notebook project_name.ipynb` in the corresponding folder.
+
+## Projects
+
+### Part of Speech Tagger 
+
+This notebook uses the [Pomegranate](https://github.com/jmschrei/pomegranate) library to build a hidden Markov model for part of speech tagging with a [universal tagset](http://www.petrovi.de/data/universal.pdf).
+
+#### Models
+1. Most Frequent Class Tagger: training accuracy: 95.72%; testing accuracy: 93.01%
+2. Hidden Markov Model Tagger: training accuracy: 97.54% testing accuracy: 95.95%
+
+### Machine Translation
+
+This notebook has different type of machine translation models starting from simple models to advanced models with good accuracy.
+The model trained on small data to translate from English language to French, it can be expanded to train on more data or translate other languages.
+
+#### Models
+Training accuracy:
+1. Simple Model: 62.12% 
+2. Embedding Model: 76.86% 
+3. Bidirectional Model: 66.50% 
+4. Encoder-Decoder Model: 61.68% 
+5. **Final Model** (Multiple Techniques): 96.74% 
+
+### Automatic Speech Recognition 
+
+Capstone project of the course with different NLP models and advanced techniques. This notebook builds a deep neural network that functions as part of an end-to-end automatic speech recognition (ASR) pipeline.
+
+![ASR Pipeline][image1]
+
+*'Reviewer feedback on Capstone Project: Impressive work in this resubmission and overall! I especially enjoyed seeing your predicted transcription which your Notebook printed in the last step "3: Obtain predictions"!'*
+
+#### Models
+
+Model 0: RNN loss: 779.6107 - val_loss: 759.2642
+Model 1: RNN + TimeDistributed Dense loss: 143.2519 - val_loss: 150.7867
+Model 2: CNN + RNN + TimeDistributed Dense loss: 107.3945 - val_loss: 145.1359
+Model 3: Deeper RNN + TimeDistributed Dense loss: 135.6761 - val_loss: 146.6482
+Model 4: Bidirectional RNN + TimeDistributed Dense loss: 159.4280 - val_loss: 162.5575
+**Final Model**: CNN + MultiHeadSelfAttention + RNN + TimeDistributed Dense loss: 94.9789 - val_loss: 137.3039
+
+![Loss][image2]
+
+## Contribution
+TODO and Implement parts in the notebook done by Zhaoning Li. Document strcure done by Udacity Team.
